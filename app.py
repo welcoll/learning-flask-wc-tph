c7f5520f7ab625e6939d13e2fa67ba922d0114a9
t.harford <t.harford@wc.school.nz>
2020-04-24 16:41:57 +1200

    Connect to database to fetch product data

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session,redirect
+import sqlite3
+from sqlite3 import Error
+
+DB_NAME = "smile.db"
 
 app = Flask(__name__)
+
+
+def create_connection(db_file):
+    """create a connection to the sqlite db"""
+    try:
+        connection = sqlite3.connect(db_file)
+        # initialise_tables(connection)
+        return connection
+    except Error as e:
+        print(e)
+
+    return None
 
 
 @app.route('/')
@@ -10,7 +26,19 @@ def render_homepage():
 
 @app.route('/menu')
 def render_menu_page():
-    return render_template('menu.html')
+
+    # connect to the database
+    con = create_connection(DB_NAME)
+
+    # SELECT the things you want from your table(s)
+    query = "SELECT name, description, volume, price, image FROM product"
+
+    cur = con.cursor()  # You need this line next
+    cur.execute(query)  # this line actually executes the query
+    product_list = cur.fetchall()  # puts the results into a list usable in python
+    con.close()
+
+    return render_template('menu.html', products=product_list)
 
 
 @app.route('/contact')
