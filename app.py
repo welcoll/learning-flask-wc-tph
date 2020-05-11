4482614040c4a703ce5e50ef3ed5a28ffbda4e21
t.harford <t.harford@wc.school.nz>
2020-05-11 15:16:06 +1200

    Add ability to register, log in and log out

--- app.py
+++ app.py
@@ -5,13 +5,13 @@ from sqlite3 import Error
 DB_NAME = "smile.db"
 
 app = Flask(__name__)
+app.secret_key = "dn493f983h45983g845h94gh583"
 
 
 def create_connection(db_file):
     """create a connection to the sqlite db"""
     try:
         connection = sqlite3.connect(db_file)
-        # initialise_tables(connection)
         return connection
     except Error as e:
         print(e)
@@ -45,18 +45,48 @@ def render_contact_page():
     return render_template('contact.html')
 
 
-@app.route('/login')
+@app.route('/login', methods=["GET", "POST"])
 def render_login_page():
-    return render_template('login.html')
+    if request.method == "POST":
+        email = request.form['email'].strip().lower()
+        password = request.form['password'].strip()
+
+        query = """SELECT id, fname, password FROM customer WHERE email = ?"""
+        con = create_connection(DB_NAME)
+        cur = con.cursor()
+        cur.execute(query, (email,))
+        user_data = cur.fetchall()
+        con.close()
+        # if given the email is not in the database this will raise an error
+        # would be better to find out how to see if the query return an empty resultset
+        try:
+            userid = user_data[0][0]
+            firstname = user_data[0][1]
+            db_password = user_data[0][2]
+        except IndexError:
+            return redirect("/login?error=Email+invalid+or+password+incorrect")
+
+        # check if the password is incorrect for that email address
+        if db_password != password:
+            return redirect("/login?error=Email+invalid+or+password+incorrect")
+        # if not bcrypt.check_password_hash(db_password, password):
+        #     return redirect(request.referrer + "?error=Email+invalid+or+password+incorrect")
+        #
+        session['email'] = email
+        session['userid'] = userid
+        session['firstname'] = firstname
+        print(session)
+        return redirect('/')
+    return render_template('login.html', logged_in=is_logged_in())
 
 
 @app.route('/signup', methods=['GET', 'POST'])
 def render_signup_page():
     if request.method == 'POST':
         print(request.form)
-        fname = request.form.get('fname')
-        lname = request.form.get('lname')
-        email = request.form.get('email')
+        fname = request.form.get('fname').strip().title()
+        lname = request.form.get('lname').strip().title()
+        email = request.form.get('email').strip().lower()
         password = request.form.get('password')
         password2 = request.form.get('password2')
 
@@ -68,14 +98,52 @@ def render_signup_page():
 
         con = create_connection(DB_NAME)
 
-        query = "INSERT INTO customer(id, fname, lname, email, password) VALUES(NULL,?,?,?,?)"
+        query = "INSERT INTO customer(id, fname, lname, email, password) " \
+                "VALUES(NULL,?,?,?,?)"
 
         cur = con.cursor()  # You need this line next
-        cur.execute(query, (fname, lname, email, password))  # this line actually executes the query
+        try:
+            cur.execute(query, (fname, lname, email, password))  # this line actually executes the query
+        except sqlite3.IntegrityError:
+            return redirect('/signup?error=Email+is+already+used')
+
         con.commit()
         con.close()
+        return redirect('/login')
 
     return render_template('signup.html')
 
 
-app.run(host='0.0.0.0')
+@app.route('/logout')
+def logout():
+    print(list(session.keys()))
+    [session.pop(key) for key in list(session.keys())]
+    print(list(session.keys()))
+    return redirect('/?message=See+you+next+time!')
+
+
+def is_logged_in():
+    if session.get("email") is None:
+        print("not logged in")
+        return False
+    else:
+        print("logged in")
+        return True
+
+    #     username = session.get("USERNAME")
+    #     user = users[username]
+    #     return render_template("public/profile.html", user=user)
+    # else:
+    #     print("No username found in session")
+    #     return redirect(url_for("sign_in"))
+    #
+    #
+    # try:
+    #     print(session['email'])
+    #     return True
+    # except KeyError:
+    #     print("NO")
+    #     return False
+
+
+app.run(host='0.0.0.0', debug=True)
